5a36cf4f42e1cc136fd1fbbd8fc2e5308e849a51
Akash Hardia <aakashhardiya@gmail.com>
2018-11-05 01:51:24 +0530

    fix: now working for no thumbnail also

--- src/SeparateBook.js
+++ src/SeparateBook.js
@@ -14,7 +14,7 @@ class SeparateBook extends Component{
           <li>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{width: 128,height: 193,backgroundImage: `url("${book.imageLinks.thumbnail}")`
+                <div className="book-cover" style={{width: 128,height: 193,backgroundImage: `url("${book.imageLinks? book.imageLinks.thumbnail: ""}}")`
                 }}></div>
                 <div className="book-shelf-changer">
                   <select onChange={this.changeShelf} value={book.shelf}>
